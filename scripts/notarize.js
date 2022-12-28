f8cf0c4a232e1cf1e4bda0307b22e5e44e5868f7
Thomas Claburn <tc@lot49.com>
2022-12-28 08:29:11 -0800

    Revert fixes due to breakage

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,39 +1,37 @@
 // See: https://medium.com/@TwitterArchiveEraser/notarize-electron-apps-7a5f988406db
 
-const fs = require("fs");
-const path = require("path");
-var electron_notarize = require("electron-notarize");
+const fs = require('fs');
+const path = require('path');
+var electron_notarize = require('electron-notarize');
 
 module.exports = async function (params) {
-	// Only notarize the app on Mac OS only.
-	if (params.electronPlatformName !== "darwin") {
-		return;
-	}
-	console.log("afterSign hook triggered", params);
+  // Only notarize the app on Mac OS only.
+  if (params.electronPlatformName !== 'darwin') {
+    return;
+  }
+  console.log('afterSign hook triggered', params);
 
-	// Same appId in electron-builder.
-	let appId = "com.lot49.rssputin";
+  // Same appId in electron-builder.
+  let appId = 'com.lot49.rssputin';
 
-	let appPath = path.join(
-		params.appOutDir,
-		`${params.packager.appInfo.productFilename}.app`
-	);
-	if (!fs.existsSync(appPath)) {
-		throw new Error(`Cannot find application at: ${appPath}`);
-	}
+  let appPath = path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`);
+  if (!fs.existsSync(appPath)) {
+    throw new Error(`Cannot find application at: ${appPath}`);
+  }
 
-	console.log(`Notarizing ${appId} found at ${appPath}`);
+  console.log(`Notarizing ${appId} found at ${appPath}`);
 
-	try {
-		await electron_notarize.notarize({
-			appBundleId: appId,
-			appPath: appPath,
-			appleId: process.env.APPLEID,
-			appleIdPassword: process.env.APPLEIDPASSRSSPUTIN,
-		});
-	} catch (error) {
-		console.error(error);
-	}
+  try {
+    await electron_notarize.notarize({
+      appBundleId: appId,
+      appPath: appPath,
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASSRSSPUTIN,
+      ascProvider: process.env.ASCPROVIDER,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 
-	console.log(`Done notarizing ${appId}`);
+  console.log(`Done notarizing ${appId}`);
 };
